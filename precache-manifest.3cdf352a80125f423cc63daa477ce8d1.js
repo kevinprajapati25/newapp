7694946e24060a68c0f305a0345c93ac35b6cf9f
kevinprajapati25 <you@example.com>
2020-08-31 14:41:27 +0530

    Updates

---
+++ precache-manifest.3cdf352a80125f423cc63daa477ce8d1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62bb24b454ef8855c790d38b793be3d0",
+    "url": "/newapp/index.html"
+  },
+  {
+    "revision": "bb874b102752c6b82577",
+    "url": "/newapp/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "dc31dc1ba3bfe1e36965",
+    "url": "/newapp/static/css/main.1a062961.chunk.css"
+  },
+  {
+    "revision": "bb874b102752c6b82577",
+    "url": "/newapp/static/js/2.a5f56984.chunk.js"
+  },
+  {
+    "revision": "7546f208d5b0b7a27f86dc0033179483",
+    "url": "/newapp/static/js/2.a5f56984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc31dc1ba3bfe1e36965",
+    "url": "/newapp/static/js/main.c51903db.chunk.js"
+  },
+  {
+    "revision": "96032b0aa0835a36963c",
+    "url": "/newapp/static/js/runtime-main.24ab0431.js"
+  },
+  {
+    "revision": "5ef09619737ae550e75d6ebf8689981e",
+    "url": "/newapp/static/media/comp1.5ef09619.jpg"
+  },
+  {
+    "revision": "ca167d2f5af58f11d9ee03711f9fa26b",
+    "url": "/newapp/static/media/comp2.ca167d2f.jpg"
+  },
+  {
+    "revision": "46b468901a1e2f0d49185b307fa24248",
+    "url": "/newapp/static/media/photo.46b46890.jpg"
+  },
+  {
+    "revision": "e20e537c6c2b03941ec703e29e20e747",
+    "url": "/newapp/static/media/photo2.e20e537c.jpg"
+  },
+  {
+    "revision": "fc93155a0a650bfbb8c5d2a21392fbad",
+    "url": "/newapp/static/media/photo2.fc93155a.png"
+  },
+  {
+    "revision": "577a6a8fc03341d23403f40bedf21c3b",
+    "url": "/newapp/static/media/photo3.577a6a8f.jpg"
+  },
+  {
+    "revision": "e0798da9ea6ce1010e857a283ecd3756",
+    "url": "/newapp/static/media/photo4.e0798da9.jpg"
+  },
+  {
+    "revision": "8dfaefac3a01d15db5740e737c32d232",
+    "url": "/newapp/static/media/photome.8dfaefac.jpg"
+  },
+  {
+    "revision": "d82d9297e1a5255ee48bd793bf9f5283",
+    "url": "/newapp/static/media/webdev.d82d9297.jpg"
+  }
+]);
